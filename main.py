631793444f0265ed7677c9014ecaa135e62ec3c5
Eytan Benittah <eytanbenittah@gmail.com>
2024-08-25 17:56:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+code = random.randint(1000,100000000001)
+
+tries = 1
+
+inputed_code = 1000
+
+while(inputed_code!= code):
+    inputed_code+=1
+    tries+=1
+print(f"The password is {inputed_code} and took {tries} tries")    
